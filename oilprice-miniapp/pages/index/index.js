a3571d99e6bc981c553f3e01aa93a86a087a5147
陈帅 <1301491593@qq.com>
2021-01-23 18:32:28 +0800

    add

--- oilprice-miniapp/pages/index/index.js
+++ oilprice-miniapp/pages/index/index.js
@@ -1,10 +1,22 @@
 // index.js
+
+const { formatTime } = require("../../utils/util")
+
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
+    nowTime: '',
+    list: []
   },
   onLoad(option) {
+    let date = new Date()
+    let nowDate = formatTime(date)
+    this.setData({
+      nowTime: nowDate, list: [
+        { title: '89#汽油' }
+      ]
+    })
   }
 })
