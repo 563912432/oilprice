65b718049de64fcb3552f4eb0f75db957e45130a
陈帅 <1301491593@qq.com>
2021-02-04 18:01:06 +0800

    add

--- oilprice-miniapp/pages/index/index.js
+++ oilprice-miniapp/pages/index/index.js
@@ -8,15 +8,16 @@ const app = getApp()
 Page({
   data: {
     nowTime: '',
-    list: []
+    list: [],
+    notice: ''
   },
   onLoad(option) {
     let date = new Date()
     let nowDate = formatTime(date)
     this.setData({
-      nowTime: nowDate, list: [
-        { title: '89#汽油' }
-      ]
+      nowTime: nowDate, 
+      list: ['6.17', '6.08', '6.52', '7.24', '5.7' ],
+      notice: '1月20日，第3个工作日，预测油价累计上调幅度50元/吨，折算为0.04元、升，调整窗口时间为：2021年1月29日24时'
     })
   }
 })
